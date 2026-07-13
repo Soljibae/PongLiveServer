35a717d70c010a7dc9739d9b6c18929d119ebaa4
Soljibae <134835785+Soljibae@users.noreply.github.com>
2026-07-13 18:37:08 +0900

    공, 패들 프리팹 추가, 임시 벽, 골존 배치
    공, 패들 프리팹 추가
    - 스폰포인트 오브젝트를 따로 배치해 추후 스폰될 위치 표시
    - 공, 패들의 스크립트 추가 추후 사용될 멤버 변수만 선언
    
    임시 벽, 골존 추가
    - 스크립트 없는 임시 배치
    
    카메라 스크립트 위치 변경

---
+++ client/PongUnity/Assets/Scripts/Game/Ball.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float radius;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ client/PongUnity/Assets/Scripts/Game/Paddle.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float width;
+    [SerializeField] private float height;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

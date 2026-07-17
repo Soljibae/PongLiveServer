13920a8dcacb237f1f484a926c5f6cf98b63eecf
Soljibae <134835785+Soljibae@users.noreply.github.com>
2026-07-17 22:42:39 +0900

    나가기 버튼 추가, 패들 스크립트 구현
    패들 스크립트 구현
    -위치 초기화 함수 및 상태별 동작 제어를 위한 bool 변수 추가 및 변경 함수 추가
    -카메라의 사이즈와 패들의 사이즈를 통한 최대, 최소 y 값 계산
    -외부 인풋을 통해 움직이는 move 함수 구현

--- client/PongUnity/Assets/Scripts/Game/Paddle.cs
+++ client/PongUnity/Assets/Scripts/Game/Paddle.cs
@@ -14,13 +14,19 @@ public class Paddle : MonoBehaviour
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
+    private float maxY, minY;
+
     public float CurrentSpeed { get; private set; }
     public float MaxSpeed => maxSpeed;
     public float Height => height;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isPlaying;
+
     void Awake()
     {
+        isPlaying = false;
+        CurrentSpeed = 0;
+
         transform.localScale = new Vector3(width, height, 1f);
 
         if (boxCollider != null)
@@ -28,11 +34,59 @@ public class Paddle : MonoBehaviour
             boxCollider.size = Vector2.one;
             boxCollider.offset = Vector2.zero;
         }
+
+        float halfCameraHeight = Camera.main.orthographicSize;
+
+        maxY = halfCameraHeight - height / 2;
+        minY = -maxY;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResetPosition()
     {
-        
+        CurrentSpeed = 0f;
+
+        rigidBody.linearVelocity = Vector2.zero;
+        rigidBody.angularVelocity = 0f;
+
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = 0f;
+
+        transform.position = currentPosition;
+    }
+    public void SetIsPlaying(bool isPlaying)
+    {
+        this.isPlaying = isPlaying;
+
+        if (!isPlaying)
+        {
+            CurrentSpeed = 0;
+            rigidBody.linearVelocity = Vector2.zero;
+            rigidBody.angularVelocity = 0f;
+        }
+    }
+
+    public void Move(float input)
+    {
+        if (!isPlaying)
+            return;
+
+        input = Mathf.Clamp(input, -1f, 1f);
+
+        if (Mathf.Approximately(input, 0f))
+        {
+            CurrentSpeed = 0f;
+        }
+        else
+        {
+            float targetSpeed = input * maxSpeed;
+
+            CurrentSpeed = Mathf.MoveTowards(CurrentSpeed, targetSpeed, acceleration * Time.fixedDeltaTime);
+        }
+
+        Vector2 nextPosition = rigidBody.position + Vector2.up * CurrentSpeed * Time.fixedDeltaTime;
+
+        nextPosition.y = Mathf.Clamp(nextPosition.y, minY, maxY);
+
+        rigidBody.MovePosition(nextPosition);
     }
 }
